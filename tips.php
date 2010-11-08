ec5af686ec48323bef599bfef4788a1e4406b54f
moha <moha@f53fe46e-8c02-459b-9fdd-93e75771b278>
2010-11-08 14:10:58 +0000

    - Multilanguage support - Internationalization
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/tips@970 f53fe46e-8c02-459b-9fdd-93e75771b278

--- tips.php
+++ tips.php
@@ -4,8 +4,8 @@ Plugin Name: Tips
 Plugin URI: 
 Description:
 Author: Andrew Billits
-Version: 1.0.1
-Author URI:
+Version: 1.0.2
+Author URI: http://incsub.com
 WDP ID: 61
 */
 
@@ -26,7 +26,9 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-$tips_current_version = '1.0.1';
+define('TIPS_LANG_DOMAIN', 'tips');
+
+$tips_current_version = '1.0.2';
 //------------------------------------------------------------------------//
 //---Config---------------------------------------------------------------//
 //------------------------------------------------------------------------//
@@ -40,6 +42,7 @@ if ($_GET['key'] == '' || $_GET['key'] === ''){
 	add_action('admin_head', 'tips_make_current');
 }
 
+add_action('init', 'tips_global_init');
 add_action('admin_menu', 'tips_plug_pages');
 if ($_GET['updated'] != 'true' && $_GET['activated'] != 'true'){
 	add_action('admin_notices', 'tips_output');
@@ -79,12 +82,25 @@ function tips_make_current() {
 
 function tips_blog_install() {
 	global $wpdb, $tips_current_version;
-	//$tips_table1 = "";
-	//$wpdb->query( $tips_table1 );
+}
+
+function tips_global_init() {
+	if (preg_match('/mu\-plugin/', PLUGINDIR) > 0) {
+		load_muplugin_textdomain(TIPS_LANG_DOMAIN, false, dirname(plugin_basename(__FILE__)).'/languages');
+	} else {
+		load_plugin_textdomain(TIPS_LANG_DOMAIN, false, dirname(plugin_basename(__FILE__)).'/languages');
+	}
 }
 
 function tips_global_install() {
 	global $wpdb, $tips_current_version;
+	
+	// Get the correct character collate
+	if ( ! empty($wpdb->charset) )
+		$charset_collate = "DEFAULT CHARACTER SET $wpdb->charset";
+	if ( ! empty($wpdb->collate) )
+		$charset_collate .= " COLLATE $wpdb->collate";
+			
 	if (get_site_option( "tips_installed" ) == '') {
 		add_site_option( 'tips_installed', 'no' );
 	}
@@ -92,31 +108,22 @@ function tips_global_install() {
 	if (get_site_option( "tips_installed" ) == "yes") {
 		// do nothing
 	} else {
-	
 		$tips_table1 = "CREATE TABLE IF NOT EXISTS `" . $wpdb->base_prefix . "tips` (
   `tip_ID` bigint(20) unsigned NOT NULL auto_increment,
   `tip_site_ID` int(20) NOT NULL default '0',
-  `tip_content` TEXT,
+  `tip_content` TEXT  CHARACTER SET utf8,
   `tip_added` varchar(255),
   PRIMARY KEY  (`tip_ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=43 ;";
-		$tips_table2 = "";
-		$tips_table3 = "";
-		$tips_table4 = "";
-		$tips_table5 = "";
+) ENGINE=MyISAM {$charset_collate};";
 
 		$wpdb->query( $tips_table1 );
-		//$wpdb->query( $tips_table2 );
-		//$wpdb->query( $tips_table3 );
-		//$wpdb->query( $tips_table4 );
-		//$wpdb->query( $tips_table5 );
 		update_site_option( "tips_installed", "yes" );
 	}
 }
 
 function tips_plug_pages() {
 	if ( is_site_admin() ){
-		add_submenu_page('ms-admin.php', 'Tips', 'Tips', 10, 'manage-tips', 'tips_manage_output' );
+		add_submenu_page('ms-admin.php', __('Tips', TIPS_LANG_DOMAIN), __('Tips', TIPS_LANG_DOMAIN), 10, 'manage-tips', 'tips_manage_output' );
 	}
 }
 
@@ -150,14 +157,14 @@ function tips_profile_option_output() {
 	global $user_id;
 	$show_tips = get_usermeta($user_id,'show_tips');
 	?>
-	<h3><?php _e('Tips') ?></h3>
+	<h3><?php _e('Tips', TIPS_LANG_DOMAIN) ?></h3>
 	<table class="form-table">
 	<tr>
-		<th><label for="show_tips"><?php _e('Show Tips') ?></label></th>
+		<th><label for="show_tips"><?php _e('Show Tips', TIPS_LANG_DOMAIN) ?></label></th>
 		<td>
 			<select name="show_tips" id="show_tips">
-				<option value="yes" <?php if ( $show_tips == '' || $show_tips == 'yes' ) { echo 'selected="selected"'; } ?> ><?php _e('Yes'); ?></option>
-				<option value="no" <?php if ( $show_tips == 'no' ) { echo 'selected="selected"'; } ?> ><?php _e('No'); ?></option>
+				<option value="yes" <?php if ( $show_tips == '' || $show_tips == 'yes' ) { echo 'selected="selected"'; } ?> ><?php _e('Yes', TIPS_LANG_DOMAIN); ?></option>
+				<option value="no" <?php if ( $show_tips == 'no' ) { echo 'selected="selected"'; } ?> ><?php _e('No', TIPS_LANG_DOMAIN); ?></option>
 			</select>
 		</td>
 	</tr>
@@ -173,11 +180,11 @@ function tips_manage_output() {
 	global $wpdb, $wp_roles, $current_site;
 	
 	if(!current_user_can('manage_options')) {
-		echo "<p>" . __('Nice Try...') . "</p>";  //If accessed properly, this message doesn't appear.
+		echo "<p>" . __('Nice Try...', TIPS_LANG_DOMAIN) . "</p>";  //If accessed properly, this message doesn't appear.
 		return;
 	}
 	if (isset($_GET['updated'])) {
-		?><div id="message" class="updated fade"><p><?php _e('' . urldecode($_GET['updatedmsg']) . '') ?></p></div><?php
+		?><div id="message" class="updated fade"><p><?php '' . urldecode($_GET['updatedmsg']) . '' ?></p></div><?php
 	}
 	echo '<div class="wrap">';
 	switch( $_GET[ 'action' ] ) {
@@ -186,11 +193,11 @@ function tips_manage_output() {
 		$tmp_tips_count = $wpdb->get_var("SELECT COUNT(*) FROM " . $wpdb->base_prefix . "tips WHERE tip_site_ID = '" . $current_site->id . "'");
 			?>
 
-            <h2><?php _e('Manage Tips') ?> (<a href="ms-admin.php?page=manage-tips&action=new_tip"><?php _e('New') ?></a>):</h2>
+            <h2><?php _e('Manage Tips', TIPS_LANG_DOMAIN) ?> (<a href="ms-admin.php?page=manage-tips&action=new_tip"><?php _e('New', TIPS_LANG_DOMAIN) ?></a>):</h2>
             <?php
 			if ($tmp_tips_count == 0){
 			?>
-            <p><?php _e('Click ') ?><a href="ms-admin.php?page=manage-tips&action=new_tip"><?php _e('here') ?></a><?php _e(' to add a new tip.') ?></p>
+            <p><?php _e('Click ', TIPS_LANG_DOMAIN) ?><a href="ms-admin.php?page=manage-tips&action=new_tip"><?php _e('here', TIPS_LANG_DOMAIN) ?></a><?php _e(' to add a new tip.', TIPS_LANG_DOMAIN) ?></p>
             <?php
 			} else {
 			$query = "SELECT tip_ID, tip_content, tip_added FROM " . $wpdb->base_prefix . "tips WHERE tip_site_ID = '" . $current_site->id . "' ORDER BY tip_ID DESC";
@@ -198,9 +205,9 @@ function tips_manage_output() {
 			echo "
 			<table cellpadding='3' cellspacing='3' width='100%' class='widefat'> 
 			<thead><tr>
-			<th scope='col'>Content</th>
-			<th scope='col'>Added</th>
-			<th scope='col'>Actions</th>
+			<th scope='col'>".__('Content', TIPS_LANG_DOMAIN)."</th>
+			<th scope='col'>".__('Added', TIPS_LANG_DOMAIN)."</th>
+			<th scope='col'>".__('Actions', TIPS_LANG_DOMAIN)."</th>
 			<th scope='col'></th>
 			</tr></thead>
 			<tbody id='the-list'>
@@ -212,8 +219,8 @@ function tips_manage_output() {
 				echo "<tr class='" . $class . "'>";
 				echo "<td valign='top'>" . $tmp_tip['tip_content'] . "</td>";
 				echo "<td valign='top'>" . date(get_option('date_format') . ' ' . get_option('time_format'),$tmp_tip['tip_added']) . "</td>";
-				echo "<td valign='top'><a href='ms-admin.php?page=manage-tips&action=edit_tip&tid=" . $tmp_tip['tip_ID'] . "' rel='permalink' class='edit'>" . __('Edit') . "</a></td>";
-				echo "<td valign='top'><a href='ms-admin.php?page=manage-tips&action=delete_tip&tid=" . $tmp_tip['tip_ID'] . "' rel='permalink' class='delete'>" . __('Remove') . "</a></td>";
+				echo "<td valign='top'><a href='ms-admin.php?page=manage-tips&action=edit_tip&tid=" . $tmp_tip['tip_ID'] . "' rel='permalink' class='edit'>" . __('Edit', TIPS_LANG_DOMAIN) . "</a></td>";
+				echo "<td valign='top'><a href='ms-admin.php?page=manage-tips&action=delete_tip&tid=" . $tmp_tip['tip_ID'] . "' rel='permalink' class='delete'>" . __('Remove', TIPS_LANG_DOMAIN) . "</a></td>";
 				echo "</tr>";
 				$class = ('alternate' == $class) ? '' : 'alternate';
 				//=========================================================//
@@ -227,18 +234,17 @@ function tips_manage_output() {
 		//---------------------------------------------------//
 		case "new_tip":
 		?>
-			<h2><?php _e('New Tip') ?></h2>
+			<h2><?php _e('New Tip', TIPS_LANG_DOMAIN) ?></h2>
             <form name="form1" method="POST" action="ms-admin.php?page=manage-tips&action=new_tip_process"> 
                 <table class="form-table">
                 <tr valign="top">
-                <th scope="row"><?php _e('Tip:') ?></th>
+                <th scope="row"><?php _e('Tip:', TIPS_LANG_DOMAIN) ?></th>
 				<td><textarea name="tip_content" id="tip_content" style="width: 95%" rows="5"><?php echo $_POST['tip_content']; ?></textarea>
-                <br />
-                <?php //_e('') ?></td> 
+                <br /></td> 
                 </tr>
                 </table>
             <p class="submit"> 
-            <input type="submit" name="Submit" value="<?php _e('Save') ?>" /> 
+            <input type="submit" name="Submit" value="<?php _e('Save', TIPS_LANG_DOMAIN) ?>" /> 
             </p> 
             </form>
         <?php
@@ -247,18 +253,17 @@ function tips_manage_output() {
 		case "new_tip_process":
 			if ($_POST['tip_content'] == ''){
 				?>
-                <h2><?php _e('New Tip') ?></h2>
+                <h2><?php _e('New Tip', TIPS_LANG_DOMAIN) ?></h2>
                 <form name="form1" method="POST" action="ms-admin.php?page=manage-tips&action=new_tip_process"> 
                     <table class="form-table">
                     <tr valign="top">
-                    <th scope="row"><?php _e('Tip') ?></th>
+                    <th scope="row"><?php _e('Tip', TIPS_LANG_DOMAIN) ?></th>
                     <td><textarea name="tip_content" id="tip_content" style="width: 95%" rows="5"><?php echo $_POST['tip_content']; ?></textarea>
-                    <br />
-                    <?php //_e('') ?></td> 
+                    <br /></td> 
                     </tr>
                     </table>
                 <p class="submit"> 
-                <input type="submit" name="Submit" value="<?php _e('Save') ?>" /> 
+                <input type="submit" name="Submit" value="<?php _e('Save', TIPS_LANG_DOMAIN) ?>" /> 
                 </p> 
                 </form>
 				<?php
@@ -266,7 +271,7 @@ function tips_manage_output() {
 				$wpdb->query( "INSERT INTO " . $wpdb->base_prefix . "tips (tip_site_ID, tip_content, tip_added) VALUES ( '" . $current_site->id . "', '" . $_POST['tip_content'] . "' , '" . time() . "')" );
 				echo "
 				<SCRIPT LANGUAGE='JavaScript'>
-				window.location='ms-admin.php?page=manage-tips&updated=true&updatedmsg=" . urlencode(__('Tip Added!')) . "';
+				window.location='ms-admin.php?page=manage-tips&updated=true&updatedmsg=" . urlencode(__('Tip Added!', TIPS_LANG_DOMAIN)) . "';
 				</script>
 				";
 			}
@@ -275,19 +280,18 @@ function tips_manage_output() {
 		case "edit_tip":
 			$tmp_tip_content = $wpdb->get_var("SELECT tip_content FROM " . $wpdb->base_prefix . "tips WHERE tip_ID = '" . $_GET['tid'] . "' AND tip_site_ID = '" . $current_site->id . "'");
 			?>
-			<h2><?php _e('Edit Tip') ?></h2>
+			<h2><?php _e('Edit Tip', TIPS_LANG_DOMAIN) ?></h2>
             <form name="form1" method="POST" action="ms-admin.php?page=manage-tips&action=edit_tip_process"> 
 			<input type="hidden" name="tid" value="<?php echo $_GET['tid']; ?>" />
                 <table class="form-table">
                 <tr valign="top">
-                <th scope="row"><?php _e('Tip') ?></th>
+                <th scope="row"><?php _e('Tip', TIPS_LANG_DOMAIN) ?></th>
 				<td><textarea name="tip_content" id="tip_content" style="width: 95%" rows="5"><?php echo $tmp_tip_content; ?></textarea>
-                <br />
-                <?php //_e('') ?></td> 
+                <br /></td> 
                 </tr>
                 </table>
             <p class="submit"> 
-            <input type="submit" name="Submit" value="<?php _e('Save Changes') ?>" /> 
+            <input type="submit" name="Submit" value="<?php _e('Save Changes', TIPS_LANG_DOMAIN) ?>" /> 
             </p> 
             </form>
 	        <?php
@@ -298,19 +302,18 @@ function tips_manage_output() {
 			if ($tmp_tip_count > 0){
 				if ($_POST['tip_content'] == ''){
 					?>
-					<h2><?php _e('Edit Tip') ?></h2>
+					<h2><?php _e('Edit Tip', TIPS_LANG_DOMAIN) ?></h2>
 					<form name="form1" method="POST" action="ms-admin.php?page=manage-tips&action=edit_tip_process"> 
 					<input type="hidden" name="tid" value="<?php echo $_POST['tid']; ?>" />
 						<table class="form-table">
 						<tr valign="top">
-						<th scope="row"><?php _e('Tip') ?></th>
+						<th scope="row"><?php _e('Tip', TIPS_LANG_DOMAIN) ?></th>
 						<td><textarea name="tip_content" id="tip_content" style="width: 95%" rows="5"><?php echo $tmp_tip_content; ?></textarea>
-						<br />
-						<?php //_e('') ?></td> 
+						<br /></td> 
 						</tr>
 						</table>
 					<p class="submit"> 
-					<input type="submit" name="Submit" value="<?php _e('Save Changes') ?>" /> 
+					<input type="submit" name="Submit" value="<?php _e('Save Changes', TIPS_LANG_DOMAIN) ?>" /> 
 					</p> 
 					</form>
 					<?php
@@ -318,7 +321,7 @@ function tips_manage_output() {
 					$wpdb->query( "UPDATE " . $wpdb->base_prefix . "tips SET tip_content = '" . $_POST['tip_content'] . "' WHERE tip_ID = '" . $_POST['tid'] . "' AND tip_site_ID = '" . $current_site->id . "'" );
 					echo "
 					<SCRIPT LANGUAGE='JavaScript'>
-					window.location='ms-admin.php?page=manage-tips&updated=true&updatedmsg=" . urlencode(__('Settings saved.')) . "';
+					window.location='ms-admin.php?page=manage-tips&updated=true&updatedmsg=" . urlencode(__('Settings saved.', TIPS_LANG_DOMAIN)) . "';
 					</script>
 					";
 				}
@@ -329,7 +332,7 @@ function tips_manage_output() {
 				$wpdb->query( "DELETE FROM " . $wpdb->base_prefix . "tips WHERE tip_ID = '" . $_GET['tid'] . "' AND tip_site_ID = '" . $current_site->id . "'" );
 				echo "
 				<SCRIPT LANGUAGE='JavaScript'>
-				window.location='ms-admin.php?page=manage-tips&updated=true&updatedmsg=" . urlencode(__('Tip Removed.')) . "';
+				window.location='ms-admin.php?page=manage-tips&updated=true&updatedmsg=" . urlencode(__('Tip Removed.', TIPS_LANG_DOMAIN)) . "';
 				</script>
 				";
 		break;
@@ -342,4 +345,3 @@ function tips_manage_output() {
 //---Support Functions----------------------------------------------------//
 //------------------------------------------------------------------------//
 
-?>
