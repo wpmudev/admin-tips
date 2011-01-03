0ae0d86f5f67c9a3e1a6fb2212ff3797587d327c
ivan <ivan@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-01-03 22:25:20 +0000

    Upgrade to WordPress 3.1
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/tips@1765 f53fe46e-8c02-459b-9fdd-93e75771b278

--- tips.php
+++ tips.php
@@ -1,16 +1,17 @@
 <?php
 /*
-Plugin Name: Tips
-Plugin URI: 
-Description:
-Author: Andrew Billits
-Version: 1.0.2
-Author URI: http://incsub.com
+Plugin Name: Admin Panel Tips
+Plugin URI: http://premium.wpmudev.org/project/admin-panel-tips
+Description: Provide your users with helpful random tips (or promotions/news) in their admin panels.
+Author: Ivan Shaovchev & Andrew Billits (Incsub)
+Version: 1.0.3
+Author URI: http://ivan.sh
+Network: true
 WDP ID: 61
 */
 
 /* 
-Copyright 2007-2009 Incsub (http://incsub.com)
+Copyright 2007-2011 Incsub (http://incsub.com)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License (Version 2 - GPLv2) as published by
@@ -28,30 +29,41 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 define('TIPS_LANG_DOMAIN', 'tips');
 
-$tips_current_version = '1.0.2';
-//------------------------------------------------------------------------//
-//---Config---------------------------------------------------------------//
-//------------------------------------------------------------------------//
+/* Get admin page location */
+if ( is_multisite() ) {
+    if ( version_compare( $wp_version, '3.0.9', '>' ) ) {
+        $tips_menu_slug = 'settings.php';
+        $tips_admin_url = admin_url('network/settings.php?page=manage-tips');
+        add_action('network_admin_menu', 'tips_plug_pages');
+    } else {
+        $tips_menu_slug = 'ms-admin.php';
+        $tips_admin_url = admin_url('ms-admin.php?page=manage-tips');
+        add_action('admin_menu', 'tips_plug_pages');
+    }
+} else {
+    $tips_menu_slug = 'settings.php';
+    $tips_admin_url = admin_url('options-general.php?page=wpmudev');
+    add_action('admin_menu', 'tips_plug_pages');
+}
+
+$tips_current_version = '1.0.3';
 $tmp_tips_prefix = "";
 $tmp_tips_suffix = "";
-//------------------------------------------------------------------------//
-//---Hook-----------------------------------------------------------------//
-//------------------------------------------------------------------------//
+
 //check for activating
 if ($_GET['key'] == '' || $_GET['key'] === ''){
 	add_action('admin_head', 'tips_make_current');
 }
-
-add_action('init', 'tips_global_init');
-add_action('admin_menu', 'tips_plug_pages');
 if ($_GET['updated'] != 'true' && $_GET['activated'] != 'true'){
 	add_action('admin_notices', 'tips_output');
+    add_action('network_admin_notices', 'tips_output');
 }
+
+add_action('init', 'tips_global_init');
 add_action('profile_personal_options', 'tips_profile_option_output');
 add_action('personal_options_update', 'tips_profile_option_update');
-//------------------------------------------------------------------------//
-//---Functions------------------------------------------------------------//
-//------------------------------------------------------------------------//
+
+
 function tips_make_current() {
 	global $wpdb, $tips_current_version;
 	if (get_site_option( "tips_version" ) == '') {
@@ -86,9 +98,9 @@ function tips_blog_install() {
 
 function tips_global_init() {
 	if (preg_match('/mu\-plugin/', PLUGINDIR) > 0) {
-		load_muplugin_textdomain(TIPS_LANG_DOMAIN, false, dirname(plugin_basename(__FILE__)).'/tips/languages');
+		load_muplugin_textdomain(TIPS_LANG_DOMAIN, false, dirname(plugin_basename(__FILE__)).'/languages');
 	} else {
-		load_plugin_textdomain(TIPS_LANG_DOMAIN, false, dirname(plugin_basename(__FILE__)).'/tips/languages');
+		load_plugin_textdomain(TIPS_LANG_DOMAIN, false, dirname(plugin_basename(__FILE__)).'/languages');
 	}
 }
 
@@ -122,8 +134,9 @@ function tips_global_install() {
 }
 
 function tips_plug_pages() {
-	if ( is_site_admin() ){
-		add_submenu_page('ms-admin.php', __('Tips', TIPS_LANG_DOMAIN), __('Tips', TIPS_LANG_DOMAIN), 10, 'manage-tips', 'tips_manage_output' );
+    global $tips_menu_slug;
+	if ( is_super_admin() ){
+		add_submenu_page( $tips_menu_slug, __('Tips', TIPS_LANG_DOMAIN), __('Tips', TIPS_LANG_DOMAIN), 10, 'manage-tips', 'tips_manage_output' );
 	}
 }
 
@@ -177,7 +190,7 @@ function tips_profile_option_output() {
 //------------------------------------------------------------------------//
 
 function tips_manage_output() {
-	global $wpdb, $wp_roles, $current_site;
+	global $wpdb, $wp_roles, $current_site, $tips_admin_url;
 	
 	if(!current_user_can('manage_options')) {
 		echo "<p>" . __('Nice Try...', TIPS_LANG_DOMAIN) . "</p>";  //If accessed properly, this message doesn't appear.
@@ -190,14 +203,14 @@ function tips_manage_output() {
 	switch( $_GET[ 'action' ] ) {
 		//---------------------------------------------------//
 		default:
-		$tmp_tips_count = $wpdb->get_var("SELECT COUNT(*) FROM " . $wpdb->base_prefix . "tips WHERE tip_site_ID = '" . $current_site->id . "'");
+            $tmp_tips_count = $wpdb->get_var("SELECT COUNT(*) FROM " . $wpdb->base_prefix . "tips WHERE tip_site_ID = '" . $current_site->id . "'");
 			?>
 
-            <h2><?php _e('Manage Tips', TIPS_LANG_DOMAIN) ?> (<a href="ms-admin.php?page=manage-tips&action=new_tip"><?php _e('New', TIPS_LANG_DOMAIN) ?></a>):</h2>
+            <h2><?php _e('Manage Tips', TIPS_LANG_DOMAIN) ?> (<a href="<?php echo $tips_admin_url; ?>&action=new_tip"><?php _e('New', TIPS_LANG_DOMAIN) ?></a>):</h2>
             <?php
 			if ($tmp_tips_count == 0){
 			?>
-            <p><?php _e('Click ', TIPS_LANG_DOMAIN) ?><a href="ms-admin.php?page=manage-tips&action=new_tip"><?php _e('here', TIPS_LANG_DOMAIN) ?></a><?php _e(' to add a new tip.', TIPS_LANG_DOMAIN) ?></p>
+            <p><?php _e('Click ', TIPS_LANG_DOMAIN) ?><a href="<?php echo $tips_admin_url; ?>&action=new_tip"><?php _e('here', TIPS_LANG_DOMAIN) ?></a><?php _e(' to add a new tip.', TIPS_LANG_DOMAIN) ?></p>
             <?php
 			} else {
 			$query = "SELECT tip_ID, tip_content, tip_added FROM " . $wpdb->base_prefix . "tips WHERE tip_site_ID = '" . $current_site->id . "' ORDER BY tip_ID DESC";
@@ -219,8 +232,8 @@ function tips_manage_output() {
 				echo "<tr class='" . $class . "'>";
 				echo "<td valign='top'>" . $tmp_tip['tip_content'] . "</td>";
 				echo "<td valign='top'>" . date(get_option('date_format') . ' ' . get_option('time_format'),$tmp_tip['tip_added']) . "</td>";
-				echo "<td valign='top'><a href='ms-admin.php?page=manage-tips&action=edit_tip&tid=" . $tmp_tip['tip_ID'] . "' rel='permalink' class='edit'>" . __('Edit', TIPS_LANG_DOMAIN) . "</a></td>";
-				echo "<td valign='top'><a href='ms-admin.php?page=manage-tips&action=delete_tip&tid=" . $tmp_tip['tip_ID'] . "' rel='permalink' class='delete'>" . __('Remove', TIPS_LANG_DOMAIN) . "</a></td>";
+				echo '<td valign="top"><a href=' . $tips_admin_url . '&action=edit_tip&tid=' . $tmp_tip['tip_ID'] . " rel='permalink' class='edit'>" . __('Edit', TIPS_LANG_DOMAIN) . "</a></td>";
+				echo '<td valign="top"><a href=' . $tips_admin_url . '&action=delete_tip&tid=' . $tmp_tip['tip_ID'] . " rel='permalink' class='delete'>" . __('Remove', TIPS_LANG_DOMAIN) . "</a></td>";
 				echo "</tr>";
 				$class = ('alternate' == $class) ? '' : 'alternate';
 				//=========================================================//
@@ -235,7 +248,7 @@ function tips_manage_output() {
 		case "new_tip":
 		?>
 			<h2><?php _e('New Tip', TIPS_LANG_DOMAIN) ?></h2>
-            <form name="form1" method="POST" action="ms-admin.php?page=manage-tips&action=new_tip_process"> 
+            <form name="form1" method="POST" action="<?php echo $tips_admin_url; ?>&action=new_tip_process">
                 <table class="form-table">
                 <tr valign="top">
                 <th scope="row"><?php _e('Tip:', TIPS_LANG_DOMAIN) ?></th>
@@ -254,7 +267,7 @@ function tips_manage_output() {
 			if ($_POST['tip_content'] == ''){
 				?>
                 <h2><?php _e('New Tip', TIPS_LANG_DOMAIN) ?></h2>
-                <form name="form1" method="POST" action="ms-admin.php?page=manage-tips&action=new_tip_process"> 
+                <form name="form1" method="POST" action="<?php echo $tips_admin_url; ?>&action=new_tip_process">
                     <table class="form-table">
                     <tr valign="top">
                     <th scope="row"><?php _e('Tip', TIPS_LANG_DOMAIN) ?></th>
@@ -271,7 +284,7 @@ function tips_manage_output() {
 				$wpdb->query( "INSERT INTO " . $wpdb->base_prefix . "tips (tip_site_ID, tip_content, tip_added) VALUES ( '" . $current_site->id . "', '" . $_POST['tip_content'] . "' , '" . time() . "')" );
 				echo "
 				<SCRIPT LANGUAGE='JavaScript'>
-				window.location='ms-admin.php?page=manage-tips&updated=true&updatedmsg=" . urlencode(__('Tip Added!', TIPS_LANG_DOMAIN)) . "';
+				window.location='" . $tips_admin_url . "&updated=true&updatedmsg=" . urlencode(__('Tip Added!', TIPS_LANG_DOMAIN)) . "';
 				</script>
 				";
 			}
@@ -281,7 +294,7 @@ function tips_manage_output() {
 			$tmp_tip_content = $wpdb->get_var("SELECT tip_content FROM " . $wpdb->base_prefix . "tips WHERE tip_ID = '" . $_GET['tid'] . "' AND tip_site_ID = '" . $current_site->id . "'");
 			?>
 			<h2><?php _e('Edit Tip', TIPS_LANG_DOMAIN) ?></h2>
-            <form name="form1" method="POST" action="ms-admin.php?page=manage-tips&action=edit_tip_process"> 
+            <form name="form1" method="POST" action="<?php echo $tips_admin_url; ?>&action=edit_tip_process">
 			<input type="hidden" name="tid" value="<?php echo $_GET['tid']; ?>" />
                 <table class="form-table">
                 <tr valign="top">
@@ -303,7 +316,7 @@ function tips_manage_output() {
 				if ($_POST['tip_content'] == ''){
 					?>
 					<h2><?php _e('Edit Tip', TIPS_LANG_DOMAIN) ?></h2>
-					<form name="form1" method="POST" action="ms-admin.php?page=manage-tips&action=edit_tip_process"> 
+					<form name="form1" method="POST" action="<?php echo $tips_admin_url; ?>&action=edit_tip_process">
 					<input type="hidden" name="tid" value="<?php echo $_POST['tid']; ?>" />
 						<table class="form-table">
 						<tr valign="top">
@@ -321,7 +334,7 @@ function tips_manage_output() {
 					$wpdb->query( "UPDATE " . $wpdb->base_prefix . "tips SET tip_content = '" . $_POST['tip_content'] . "' WHERE tip_ID = '" . $_POST['tid'] . "' AND tip_site_ID = '" . $current_site->id . "'" );
 					echo "
 					<SCRIPT LANGUAGE='JavaScript'>
-					window.location='ms-admin.php?page=manage-tips&updated=true&updatedmsg=" . urlencode(__('Settings saved.', TIPS_LANG_DOMAIN)) . "';
+					window.location='" . $tips_admin_url . "&updated=true&updatedmsg=" . urlencode(__('Settings saved.', TIPS_LANG_DOMAIN)) . "';
 					</script>
 					";
 				}
@@ -332,7 +345,7 @@ function tips_manage_output() {
 				$wpdb->query( "DELETE FROM " . $wpdb->base_prefix . "tips WHERE tip_ID = '" . $_GET['tid'] . "' AND tip_site_ID = '" . $current_site->id . "'" );
 				echo "
 				<SCRIPT LANGUAGE='JavaScript'>
-				window.location='ms-admin.php?page=manage-tips&updated=true&updatedmsg=" . urlencode(__('Tip Removed.', TIPS_LANG_DOMAIN)) . "';
+				window.location='" . $tips_admin_url . "&updated=true&updatedmsg=" . urlencode(__('Tip Removed.', TIPS_LANG_DOMAIN)) . "';
 				</script>
 				";
 		break;
@@ -341,7 +354,14 @@ function tips_manage_output() {
 	echo '</div>';
 }
 
-//------------------------------------------------------------------------//
-//---Support Functions----------------------------------------------------//
-//------------------------------------------------------------------------//
-
+/*
+ * Update Notifications Notice
+ */
+if ( !function_exists( 'wdp_un_check' ) ):
+function wdp_un_check() {
+    if ( !class_exists('WPMUDEV_Update_Notifications') && current_user_can('edit_users') )
+        echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
+}
+add_action( 'admin_notices', 'wdp_un_check', 5 );
+add_action( 'network_admin_notices', 'wdp_un_check', 5 );
+endif; 
